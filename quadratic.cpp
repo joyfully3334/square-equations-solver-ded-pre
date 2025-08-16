6394317bb7211f0b1b4220cdfafe1fe0938ddada
joyfully3334 <mikhail5768@inbox.ru>
2025-08-16 22:30:45 +0300

    Another naming edits
    Now functions has Pascal_Snake_Case!

--- quadratic.cpp
+++ quadratic.cpp
@@ -11,43 +11,43 @@ enum AMOUNT_OF_SOLUTIONS {
 };
 const double EPS = 1e-5;
 
-int read_coef(double *coef, char name);
+int Read_Coef(double *coef, char name);
 
-int read_input(double *a, double *b, double *c);
+int Read_Input(double *a, double *b, double *c);
 
-int coef_err_handler(int read_result);
+int Coef_Err_Handler(int read_result);
 
-int zero_comp(double num);
+int Zero_Comp(double num);
 
-int less_then_zero(double num);
+int Less_Then_Zero(double num);
 
-void solve_quadratic(double a, double b, double c,
+void Solve_Quadratic(double a, double b, double c,
                      double *x1, double *x2, int *solutions);
 
 void print_result(double x1, double x2, int solutions);
 
-struct quadratic {
+struct SquareEquation {
   double a, b, c, x1, x2;
-  int solutions;
+  int number_of_solutions;
 };
 
 int main() {
-  struct quadratic quad1 = {0., 0., 0., 0., 0., 0};
-  if (coef_err_handler(read_input(&quad1.a, &quad1.b, &quad1.c)))
+  struct SquareEquation quad1 = {0., 0., 0., 0., 0., 0};
+  if (Coef_Err_Handler(Read_Input(&quad1.a, &quad1.b, &quad1.c)))
     return 1;
 
   printf("Solving equation: %lfx^2 %c %lfx %c %lf = 0\n", quad1.a,
          (quad1.b >= 0 ? '+' : '-'), fabs(quad1.b),
          (quad1.c >= 0 ? '+' : '-'), fabs(quad1.c));
 
-  solve_quadratic(quad1.a, quad1.b, quad1.c, &quad1.x1, &quad1.x2,
-                  &quad1.solutions);
-  print_result(quad1.x1, quad1.x2, quad1.solutions);
+  Solve_Quadratic(quad1.a, quad1.b, quad1.c, &quad1.x1, &quad1.x2,
+                  &quad1.number_of_solutions);
+  print_result(quad1.x1, quad1.x2, quad1.number_of_solutions);
 
   return 0;
 }
 
-int read_coef(double *coef, char name) {
+int Read_Coef(double *coef, char name) {
   printf("Enter %c: ", name);
   if (!scanf("%lf", coef))
     return not_a_number;
@@ -56,20 +56,20 @@ int read_coef(double *coef, char name) {
   return 0;
 }
 
-int read_input(double *a, double *b, double *c) {
-  int ret_val = read_coef(a, 'a');
+int Read_Input(double *a, double *b, double *c) {
+  int ret_val = Read_Coef(a, 'a');
   if (ret_val)
     return ret_val;
-  ret_val = read_coef(b, 'b');
+  ret_val = Read_Coef(b, 'b');
   if (ret_val)
     return ret_val;
-  ret_val = read_coef(c, 'c');
+  ret_val = Read_Coef(c, 'c');
   if (ret_val)
     return ret_val;
   return 0;
 }
 
-int coef_err_handler(int read_result) {
+int Coef_Err_Handler(int read_result) {
   if (read_result == not_a_number) {
     printf("Incorrect input data: Provided symbols is not a number\n");
     return 1;
@@ -80,17 +80,17 @@ int coef_err_handler(int read_result) {
   return 0;
 }
 
-int zero_comp(double num) { return (fabs(num - 0.) <= EPS); }
+int Zero_Comp(double num) { return (fabs(num - 0.) <= EPS); }
 
-int less_then_zero(double num) { return (num < -EPS); }
+int Less_Then_Zero(double num) { return (num < -EPS); }
 
-void solve_quadratic(double a, double b, double c, double *x1, double *x2,
+void Solve_Quadratic(double a, double b, double c, double *x1, double *x2,
                      int *solutions) {
   double diskr = b * b - 4 * a * c;
   double sq_diskr = sqrt(diskr);
-  if (zero_comp(a)) {
-    if (zero_comp(b)) {
-      if (zero_comp(b))
+  if (Zero_Comp(a)) {
+    if (Zero_Comp(b)) {
+      if (Zero_Comp(b))
         *solutions = inf_solutions;
       else
         *solutions = zero_solutions;
@@ -98,9 +98,9 @@ void solve_quadratic(double a, double b, double c, double *x1, double *x2,
       *x1 = -(double)c / b;
       *solutions = one_solution;
     }
-  } else if (less_then_zero(diskr)) {
+  } else if (Less_Then_Zero(diskr)) {
     *solutions = zero_solutions;
-  } else if (zero_comp(diskr)) {
+  } else if (Zero_Comp(diskr)) {
     *x1 = -(double)b / 2 / a;
     *solutions = one_solution;
   } else {

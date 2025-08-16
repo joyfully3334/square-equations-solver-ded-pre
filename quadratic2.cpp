1b14ca13bf0b6840e9fe0f0581577f5ab0cd198a
joyfully3334 <mikhail5768@inbox.ru>
2025-08-16 14:20:12 +0300

    Rewrite file
    Now the only difference between this file and another one is in the
    function called "read_coef".

--- quadratic2.cpp
+++ quadratic2.cpp
@@ -3,24 +3,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_LEN 81
-#define EPS 1e-5
+int input_coef(double *coef, char name);
 
-void read_coef(double *a, double *b, double *c);
+int zero_comp(double num, const double EPS);
+
+int read_coef(double *a, double *b, double *c);
 
 int main() {
+  const double EPS = 1e-5;
   double a = 0., b = 0., c = 0.;
-  read_coef(&a, &b, &c);
+  if (read_coef(&a, &b, &c)) {
+    printf("ERROR: Incorrect input data\n");
+    exit(EXIT_FAILURE);
+  }
 
   printf("Solving equation: %lfx^2 %c %lfx %c %lf = 0\n", a,
-         (b >= 0 ? '+' : '-'), abs(b),
-         (c >= 0 ? '+' : '-'), abs(c));
+         (b >= 0 ? '+' : '-'), fabs(b),
+         (c >= 0 ? '+' : '-'), fabs(c));
 
-  int diskr = b * b - 4 * a * c;
+  double diskr = b * b - 4 * a * c;
   double sq_diskr = sqrt(diskr);
-  if (fabs(a - 0) <= EPS) {
-    if (b == 0) {
-      if (c == 0)
+  if (zero_comp(a, EPS)) {
+    if (zero_comp(b, EPS)) {
+      if (zero_comp(b, EPS))
         printf("Every possible x is allowed for this equation\n");
       else
         printf("There is no solution for this equation\n");
@@ -28,9 +33,9 @@ int main() {
       double x = -(double)c / b;
       printf("Solution has found: x = %lf\n", x);
     }
-  } else if (diskr < 0) {
+  } else if (diskr < -EPS) {
     printf("There is no solution for this square equation\n");
-  } else if (diskr == 0) {
+  } else if (zero_comp(diskr, EPS)) {
     double x = -(double)b / 2 / a;
     printf("Solution has found: x = %lf\n", x);
   } else {
@@ -42,7 +47,20 @@ int main() {
   return 0;
 }
 
-void read_coef(double *a, double *b, double *c) {
+int zero_comp(double num, const double EPS) {
+  return (fabs(num - 0.) <= EPS);
+}
+
+int input_coef(double *coef, char name) {
+  printf("Enter %c: ", name);
+  if (!scanf("%lf", coef) || !isfinite(*coef)) {
+    return 1;
+  }
+  return 0;
+}
+
+int read_coef(double *a, double *b, double *c) {
+  const int MAX_LEN = 81;
   char input[MAX_LEN];
   fputs("Enter square equation (format: ax^2 + bx + c): ", stdout);
   fgets(input, MAX_LEN, stdin);
@@ -67,4 +85,5 @@ void read_coef(double *a, double *b, double *c) {
   *c = nums[0];
   *b = nums[1];
   *a = nums[2];
+  return 0;
 }

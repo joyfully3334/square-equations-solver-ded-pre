76e4130be1bf2c943d5c897ced671b9383812d56
joyfully3334 <mikhail5768@inbox.ru>
2025-08-17 23:34:11 +0300

    Sync with another file
    As I said before, second file differ from first only in ReadInput
    function. Another part is now synced.

--- quadratic2.cpp
+++ quadratic2.cpp
@@ -1,9 +1,10 @@
+#include <assert.h>
 #include <ctype.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-enum INPUT_ERRORS { not_a_number = 1, not_finite = 2 };
+enum INPUT_ERRORS { no_input_errors = 0, not_a_number = 1, not_finite = 2 };
 enum AMOUNT_OF_SOLUTIONS {
   zero_solutions = 0,
   one_solution   = 1,
@@ -12,52 +13,54 @@ enum AMOUNT_OF_SOLUTIONS {
 };
 const double EPS = 1e-5;
 
-int Read_Coef(double *coef, char name);
-
-int Read_Input(double *a, double *b, double *c);
-
-int Coef_Err_Handler(int read_result);
-
-int Zero_Comp(double num);
-
-int Less_Then_Zero(double num);
-
-void Solve_Quadratic(double a, double b, double c,
-                     double *x1, double *x2, int *solutions);
-
-void print_result(double x1, double x2, int solutions);
-
 struct SquareEquation {
   double a, b, c, x1, x2;
-  int number_of_solutions;
+  AMOUNT_OF_SOLUTIONS number_of_solutions;
 };
 
+INPUT_ERRORS ReadCoef(double *coef, char name);
+
+INPUT_ERRORS ReadInput(SquareEquation *quad);
+
+int CoefErrHandler(INPUT_ERRORS read_result);
+
+int ZeroComp(double num);
+
+void SolveEquation(SquareEquation *quad);
+
+void SolveQuadratic(SquareEquation *quad);
+
+void SolveLinear(SquareEquation *quad);
+
+int PrintResult(SquareEquation quad);
+
 int main() {
-  struct SquareEquation quad1 = {0., 0., 0., 0., 0., 0};
-  if (Coef_Err_Handler(Read_Input(&quad1.a, &quad1.b, &quad1.c)))
+  SquareEquation quad1 = {0., 0., 0., 0., 0., zero_solutions};
+  if (CoefErrHandler(ReadInput(&quad1)))
     return 1;
 
   printf("Solving equation: %lfx^2 %c %lfx %c %lf = 0\n", quad1.a,
          (quad1.b >= 0 ? '+' : '-'), fabs(quad1.b),
          (quad1.c >= 0 ? '+' : '-'), fabs(quad1.c));
 
-  Solve_Quadratic(quad1.a, quad1.b, quad1.c, &quad1.x1, &quad1.x2,
-                  &quad1.number_of_solutions);
-  print_result(quad1.x1, quad1.x2, quad1.number_of_solutions);
+  SolveEquation(&quad1);
+  if (PrintResult(quad1))
+    return 1;
 
   return 0;
 }
 
-int Read_Coef(double *coef, char name) {
+INPUT_ERRORS ReadCoef(double *coef, char name) {
+  assert(coef != NULL);
   printf("Enter %c: ", name);
   if (!scanf("%lf", coef))
     return not_a_number;
   if (!isfinite(*coef))
     return not_finite;
-  return 0;
+  return no_input_errors;
 }
 
-int Read_Input(double *a, double *b, double *c) {
+INPUT_ERRORS ReadInput(SquareEquation *quad) {
   const int MAX_LEN = 81;
   char input[MAX_LEN];
   printf("Enter square equation (format: ax^2 + bx + c): ");
@@ -88,13 +91,13 @@ int Read_Input(double *a, double *b, double *c) {
         break;
     }
   }
-  *c = nums[0];
-  *b = nums[1];
-  *a = nums[2];
-  return 0;
+  quad->c = nums[0];
+  quad->b = nums[1];
+  quad->a = nums[2];
+  return no_input_errors;
 }
 
-int Coef_Err_Handler(int read_result) {
+int CoefErrHandler(INPUT_ERRORS read_result) {
   if (read_result == not_a_number) {
     printf("Incorrect input data: Provided symbols is not a number\n");
     return 1;
@@ -105,43 +108,70 @@ int Coef_Err_Handler(int read_result) {
   return 0;
 }
 
-int Zero_Comp(double num) { return (fabs(num - 0.) <= EPS); }
+int ZeroComp(double num) {
+  if (num < -EPS)
+    return -1;
+  else if (fabs(num - 0.) <= EPS)
+    return 0;
+  else
+    return 1;
+}
 
-int Less_Then_Zero(double num) { return (num < -EPS); }
 
-void Solve_Quadratic(double a, double b, double c, double *x1, double *x2,
-                     int *solutions) {
-  double diskr = b * b - 4 * a * c;
+void SolveEquation(SquareEquation *quad) {
+  assert(quad != NULL);
+  if (ZeroComp(quad->a) == 0)
+    SolveLinear(quad);
+  else
+    SolveQuadratic(quad);
+}
+
+void SolveQuadratic(SquareEquation *quad) {
+  assert(quad != NULL);
+  double diskr = quad->b * quad->b - 4 * quad->a * quad->c;
   double sq_diskr = sqrt(diskr);
-  if (Zero_Comp(a)) {
-    if (Zero_Comp(b)) {
-      if (Zero_Comp(b))
-        *solutions = inf_solutions;
-      else
-        *solutions = zero_solutions;
-    } else {
-      *x1 = -(double)c / b;
-      *solutions = one_solution;
-    }
-  } else if (Less_Then_Zero(diskr)) {
-    *solutions = zero_solutions;
-  } else if (Zero_Comp(diskr)) {
-    *x1 = -(double)b / 2 / a;
-    *solutions = one_solution;
+  if (ZeroComp(diskr) < 0) {
+    quad->number_of_solutions = zero_solutions;
+  } else if (ZeroComp(diskr) == 0) {
+    quad->x1 = -quad->b / 2 / quad->a;
+    quad->number_of_solutions = one_solution;
   } else {
-    *x1 = (-b - sq_diskr) / 2 / a;
-    *x2 = (-b + sq_diskr) / 2 / a;
-    *solutions = two_solutions;
+    quad->x1 = (-quad->b - sq_diskr) / 2 / quad->a;
+    quad->x2 = (-quad->b + sq_diskr) / 2 / quad->a;
+    quad->number_of_solutions = two_solutions;
   }
 }
 
-void print_result(double x1, double x2, int solutions) {
-  if (solutions == zero_solutions)
-    printf("There is no solution for this square equation\n");
-  else if (solutions == one_solution)
-    printf("Solution has found: x = %lf\n", x1);
-  else if (solutions == two_solutions)
-    printf("Solution has found: x1 = %lf, x2 = %lf\n", x1, x2);
-  else if (solutions == inf_solutions)
-    printf("Every possible x is allowed for this equation\n");
+void SolveLinear(SquareEquation *quad) {
+  assert(quad != NULL);
+  if (ZeroComp(quad->b) == 0) {
+    if (ZeroComp(quad->c) == 0)
+      quad->number_of_solutions = inf_solutions;
+    else
+      quad->number_of_solutions = zero_solutions;
+  } else {
+    quad->x1 = -quad->c / quad->b;
+    quad->number_of_solutions = one_solution;
+  }
+}
+
+int PrintResult(SquareEquation quad) {
+  switch (quad.number_of_solutions) {
+    case zero_solutions: 
+      printf("There is no solution for this square equation\n");
+      break;
+    case one_solution:
+      printf("Solution has found: x = %lf\n", quad.x1);
+      break;
+    case two_solutions:
+      printf("Solution has found: x1 = %lf, x2 = %lf\n", quad.x1, quad.x2);
+      break;
+    case inf_solutions:
+      printf("Every possible x is allowed for this equation\n");
+      break;
+    default:
+      printf("An error occured in PrintResult function");
+      return 1;
+  }
+  return 0;
 }

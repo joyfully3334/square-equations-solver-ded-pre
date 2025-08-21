f77d0848afda7f48deabeb24b7f890a99e999675
joyfully3334 <mikhail5768@inbox.ru>
2025-08-21 14:49:01 +0300

    Change build.sh to actually build programm

--- build.sh
+++ build.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-# Format: .../ccm file1 file2 ... fileN -a arg1 arg2 ... argN
+# Format: build.sh file1 file2 ... fileN
 
 file=""
 flags="-D _DEBUG -ggdb3 -std=c++17 -O0 -Wall -Wextra -Weffc++ -Waggressive-loop-optimizations -Wc++14-compat -Wmissing-declarations -Wcast-align -Wcast-qual -Wchar-subscripts -Wconditionally-supported -Wconversion -Wctor-dtor-privacy -Wempty-body -Wfloat-equal -Wformat-nonliteral -Wformat-security -Wformat-signedness -Wformat=2 -Winline -Wlogical-op -Wnon-virtual-dtor -Wopenmp-simd -Woverloaded-virtual -Wpacked -Wpointer-arith -Winit-self -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=2 -Wsuggest-attribute=noreturn -Wsuggest-final-methods -Wsuggest-final-types -Wsuggest-override -Wswitch-default -Wswitch-enum -Wsync-nand -Wundef -Wunreachable-code -Wunused -Wuseless-cast -Wvariadic-macros -Wno-literal-suffix -Wno-missing-field-initializers -Wno-narrowing -Wno-old-style-cast -Wno-varargs -Wstack-protector -fcheck-new -fsized-deallocation -fstack-protector -fstrict-overflow -flto-odr-type-merging -fno-omit-frame-pointer -Wlarger-than=8192 -Wstack-usage=8192 -pie -fPIE -Werror=vla -fsanitize=address,alignment,bool,bounds,enum,float-cast-overflow,float-divide-by-zero,integer-divide-by-zero,leak,nonnull-attribute,null,object-size,return,returns-nonnull-attribute,shift,signed-integer-overflow,undefined,unreachable,vla-bound,vptr"
 
-echo $(echo "$*" | awk -F'-a ' '{print $1}') "${flags}" | xargs g++ -o ./.tmp.out
+echo "$*" "${flags}" | xargs g++ -o ./quadratic >./.tmp_error_log.txt 2>&1
 
-if [[ $? != 0 ]]; then
-  exit 1;
-fi
-
-if [[ "$*" =~ .*-a.+ ]]; then
-  trap 'rm ./.tmp.out >/dev/null 2>&1; exit 1' SIGKILL SIGTERM SIGINT
-  echo "$*" | rev | awk -F' a-' '{print $1}' | rev | xargs ./.tmp.out
+if [[ "$?" -eq 0 ]]; then
+  echo Programm successfully builded to ./quadratic
 else
-  trap 'rm ./.tmp.out >/dev/null 2>&1; exit 1' SIGKILL SIGTERM SIGINT
-  ./.tmp.out
+  echo An error occurred. You can read error log in ./error_log.txt
+  cat ./.tmp_error_log.txt > ./error_log.txt
 fi
 
-rm ./.tmp.out >/dev/null 2>&1
+rm -f ./.tmp_error_log.txt

f6aeb399e3367a75418e1cef153159efe1328985
Mikhail <mikhail5768@inbox.ru>
2025-08-24 12:14:29 +0300

    Refactor main()

--- quadratic_main.cpp
+++ quadratic_main.cpp
@@ -21,25 +21,24 @@ int main(int argc, char **argv) {
     return ExecuteTests();
   } else { // no --self-check flag
     SquareEquation quad1;
+
     if (CheckFlag(argc, argv, "--parse")) {
       if (InputErrorHandler(ParseInput(&quad1)))
         return 1;
-    } else {
+    } else { // no --parse flag
       if (InputErrorHandler(ReadInput(&quad1)))
         return 1;
     }
-
 
     printf("Solving equation: %lgx^2 %c %lgx %c %lg = 0\n", quad1.a,
           (quad1.b >= 0 ? '+' : '-'), fabs(quad1.b),
           (quad1.c >= 0 ? '+' : '-'), fabs(quad1.c));
 
     SolveEquation(&quad1);
+
     if (PrintResult(quad1))
       return 1;
+  }
 
-    return 0;
-}
-
-return 0;
+  return 0;
 }

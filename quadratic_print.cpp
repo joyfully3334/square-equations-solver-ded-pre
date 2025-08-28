6207d183c14658da4f8a95e6714ad78411773126
joyfully3334 <mikhail5768@inbox.ru>
2025-08-28 17:13:32 +0300

    Update version information

--- quadratic_print.cpp
+++ quadratic_print.cpp
@@ -29,7 +29,7 @@ PRINT_RESULTS_ERRORS PrintResult(const SquareEquation quad) {
 }
 
 void PrintWelcome() {
-  printf("%s\n%s\n", "\nSquare equation solver", "Version from 25.08.2025\n");
+  printf("%s\n%s\n", "\nSquare equation solver", "Version from 28.08.2025\n");
 }
 void PrintHelp() {
     printf("%s\n%s\n%s\n%s\n%s\n%s\n",
